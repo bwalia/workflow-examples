e71da42a7eaeefb81b6734b1daf0ae3959cacdba
Balinder Walia <balinder.walia@gmail.com>
2023-02-20 14:59:54 +0100

    Workflow to bootstrap terraform

--- workflow.sh
+++ workflow.sh
@@ -3,6 +3,11 @@
 echo "My Secret: $1"
 echo "Cloud provide name: $2"
 
+if [ -z $2 ]; then
+  echo "Cloud provider name is not set"
+  exit 1
+fi
+
 docker build -t terraform_runner .
 docker run terraform_runner
 
