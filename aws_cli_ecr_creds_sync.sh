7b7b958495293af751c96034c7a1de20bddafa67
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-03-01 20:53:18 +0000

    Workflow structure fixed

--- aws_cli_ecr_creds_sync.sh
+++ aws_cli_ecr_creds_sync.sh
@@ -10,12 +10,12 @@ aws_region=$4
 docker build -f Dockerfile_ecr_creds_sync -t ecr_creds_sync . --no-cache
 
 docker run \
--e "AWS_ACCOUNT_ID=${aws_account_id}" \
--e "AWS_ACCESS_KEY_ID=${aws_access_key_id}" \
--e "AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}" \
--e "AWS_DEFAULT_REGION=${aws_region}" \
--v "AWS_ACCOUNT_ID=${aws_account_id}" \
--v "AWS_ACCESS_KEY_ID=${aws_access_key_id}" \
--v "AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}" \
--v "AWS_DEFAULT_REGION=${aws_region}" ecr_creds_sync
+-e AWS_ACCOUNT_ID="${aws_account_id}" \
+-e AWS_ACCESS_KEY_ID="${aws_access_key_id}" \
+-e AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}" \
+-e AWS_DEFAULT_REGION="${aws_region}" \
+-v AWS_ACCOUNT_ID="${aws_account_id}" \
+-v AWS_ACCESS_KEY_ID="${aws_access_key_id}" \
+-v AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}" \
+-v AWS_DEFAULT_REGION="${aws_region}" ecr_creds_sync
 
