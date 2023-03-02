2c5eba4e706fd2a7a774197f363699f82324a00b
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-03-02 16:28:14 +0000

    Restructured the params as env vars

--- aws_cli_ecr_creds_sync.sh
+++ aws_cli_ecr_creds_sync.sh
@@ -2,20 +2,43 @@
 
 set -x
 
-aws_account_id=$1
-aws_access_key_id=$2
-aws_secret_access_key=$3
-aws_region=$4
+if [ -z "$1" ];
+then
+AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID}
+else
+      AWS_ACCOUNT_ID=$1
+fi
+
+if [ -z "$2" ];
+then
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+else
+      AWS_ACCESS_KEY_ID=$2
+fi
+
+if [ -z "$3" ];
+then
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+else
+      AWS_SECRET_ACCESS_KEY=$3
+fi
+
+if [ -z "$4" ];
+then
+AWS_DEFAULT_REGION=${AWS_REGION}
+else
+      AWS_DEFAULT_REGION=$4
+fi
 
 docker build -f Dockerfile_ecr_creds_sync -t ecr_creds_sync . --no-cache
 
 docker run \
--e AWS_ACCOUNT_ID="${aws_account_id}" \
--e AWS_ACCESS_KEY_ID="${aws_access_key_id}" \
--e AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}" \
--e AWS_DEFAULT_REGION="${aws_region}" \
--v AWS_ACCOUNT_ID="${aws_account_id}" \
--v AWS_ACCESS_KEY_ID="${aws_access_key_id}" \
--v AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}" \
--v AWS_DEFAULT_REGION="${aws_region}" ecr_creds_sync
+-e AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID}" \
+-e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+-e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+-e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
+-v AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID}" \
+-v AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+-v AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+-v AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" ecr_creds_sync
 
