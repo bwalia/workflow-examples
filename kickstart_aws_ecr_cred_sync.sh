4fc5d3c955df9820da05215fc86d5bb730566570
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-03-02 19:01:40 +0000

    K3S config udpated and both cluster should have ecr secret

--- kickstart_aws_ecr_cred_sync.sh
+++ kickstart_aws_ecr_cred_sync.sh
@@ -5,9 +5,6 @@ set -x
 echo "Deploying AWS ECR reg creds in kubernetes clusters:"
 #AWS_PROFILE=default
 
-ls -latr
-pwd
-
 aws sts get-caller-identity
 
 ACCOUNTID=$(AWS_PROFILE=$AWS_PROFILE aws sts get-caller-identity | jq ".Account" -r)
@@ -20,6 +17,8 @@ TOKEN=`aws ecr --region=$REGION get-authorization-token --output text \
     --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
 
 echo $KUBE_CONFIG_DATA_K3S1 | base64 -d > k3s1.yaml
+ls -latr
+
 export KUBECONFIG=k3s1.yaml
 
 kubectl version
@@ -32,6 +31,7 @@ kubectl create secret docker-registry $SECRET_NAME \
     --docker-email="$EMAIL"
 
 echo $KUBE_CONFIG_DATA_K3S2 | base64 -d > k3s2.yaml
+ls -latr
 export KUBECONFIG=k3s2.yaml
 
 kubectl version
